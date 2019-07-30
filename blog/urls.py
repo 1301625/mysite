c6dd391a1d30fc6ab4959a90e8afa1afb38b56c9
hoonha <sosqsz2@naver.com>
2019-07-31 03:40:48 +0900

    삭제 기능 수정

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('new/', views.post_new, name= 'post_new'),
     path('<int:pk>/',views.post_detail, name='post_detail'),
     path('<int:pk>/edit',views.post_edit, name='post_edit'),
-    path('<int:pk>/',views.post_remove, name='post_remove'),
+    path('<int:pk>/remove',views.post_remove, name='post_remove'),
     path('<int:pk>/published',views.post_publish, name='post_publish'),
     path('<tags>/', views.post_tags, name='post_tags'),
 ]
