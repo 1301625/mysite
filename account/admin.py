859f61e972548f207976adb3c5ab68539f643c6c
hoonha <sosqsz2@naver.com>
2019-08-01 17:15:47 +0900

    login, signup 폼 변경 디자인 변경

--- account/admin.py
+++ account/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Account
 # Register your models here.
 from account import forms
+
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
 
